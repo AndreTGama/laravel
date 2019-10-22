ef28b5bad394f1d7052cc1a55e8a15a2c098d6cd
AndreTGama <andre.gama@nexttecnologiadainformacao.com.br>
2019-10-22 16:08:54 -0300

    mudando o nome dos arquivos para nao correr risco de ter nomes iguais

--- app/Http/Controllers/NoticiasController.php
+++ app/Http/Controllers/NoticiasController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class NoticiasController extends Controller
 {
@@ -15,21 +14,18 @@ class NoticiasController extends Controller
     }
     public function criarNoticia(Request $req){
         $image = $req->file('arquivo');
-        $fileCount = count($image);
-        $uploadcount = 0;
-
         if (empty($image)) {
             abort(400, 'Nenhum arquivo foi enviado.');
         }else{
             foreach($image as $img){
-                if($img){
-                    $destinationPath = 'uploads';
-                    $filename = $img->getClientOriginalName();
-                    $upload_success = $img->move($destinationPath, $filename);
-                    $uploadcount ++;
-                }
+                $withAccent = array('à','á','â','ã','ä','å','ç','è','é','ê','ë','ì','í','î','ï','ñ','ò','ó','ô','õ','ö','ù','ü','ú','ÿ','À','Á','Â','Ã','Ä','Å','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ñ','Ò','Ó','Ô','Õ','Ö','O','Ù','Ü','Ú','Ÿ',' ','-',);
+                $withoutAccent = array('a','a','a','a','a','a','c','e','e','e','e','i','i','i','i','n','o','o','o','o','o','u','u','u','y','A','A','A','A','A','A','C','E','E','E','E','I','I','I','I','N','O','O','O','O','O','O','U','U','U','Y','_','-',);
+                $decodedName = str_replace($withAccent, $withoutAccent, $img->getClientOriginalName());
+                $destinationPath = 'uploads';
+                $filename = rand().'-'.$decodedName;
+                $img->move($destinationPath, $filename);
             }
             return ('sucesso');
-        }
+        }               
     }
 }
