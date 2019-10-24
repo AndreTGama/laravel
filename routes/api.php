41e01e05a538445d438c8a01f0d44cfd9b304477
AndreTGama <andre.gama@nexttecnologiadainformacao.com.br>
2019-10-24 16:08:20 -0300

    fazendo group de routes na api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
-
-Route::get('/noticias', ['as' => 'noticias', 'uses' => 'NoticiasController@index']);
-Route::get('/nova-noticia', ['as' => 'noticias.nova', 'uses' => 'NoticiasController@indexNovaNoticia']);
-Route::post('/nova/noticia', ['as' => 'noticias.nova.post', 'uses' => 'NoticiasController@criarNoticia']);
-Route::get('/nova/noticia={id}', ['as' => 'pagina.noticia', 'uses' => 'NoticiasController@paginaNoticia']);
+Route::group(['prefix' => 'noticias'],function (){
+    Route::get('todas', ['as' => 'todas.noticias','uses' => 'NoticiasController@index']);
+    Route::get('nova-noticia', ['as' => 'noticia.nova', 'uses' => 'NoticiasController@indexNovaNoticia']);
+    Route::post('nova/noticia', ['as' => 'noticia.nova.post', 'uses' => 'NoticiasController@criarNoticia']);
+    Route::get('nova/noticia={id}', ['as' => 'pagina.noticia', 'uses' => 'NoticiasController@paginaNoticia']);
+});
