ef0123ad60a9fc1913fb7dd97be77dc1c06edb24
AndreTGama <andre.gama@nexttecnologiadainformacao.com.br>
2019-10-28 12:33:06 -0300

    corrigindo os merges

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -1,31 +1,30 @@
 @extends('layout.site')
 @section('titulo','Login')
 @section('conteudo')
-<div class="container">
-  <div class="card" id="cardLogin">
-    <div id="cardLoginBody">
-      <form action="{{route('cadastro.post')}}" method="POST">
-        @csrf
-          <div class="form-group">
-            <label for="exampleInputEmail1">Nome</label>
-            <input type="text" class="form-control" name="lblNome" id="lblNome" aria-describedby="emailHelp" placeholder="Nome" required>
+    <div class="container">
+        <div class="card" id="cardLogin">
+          <div id="cardLoginBody">
+              <form>
+                    <div class="form-group">
+                            <label for="exampleInputEmail1">Nome</label>
+                            <input type="email" class="form-control" name="lblNome" id="lblNome" aria-describedby="emailHelp" placeholder="Nome" required>
+                          </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email</label>
+                    <input type="email" class="form-control" id="lblEmail" name="lblEmail" aria-describedby="emailHelp" placeholder="E-mail" required>
+                    <small id="emailHelp" class="form-text text-muted">Nunca compartilharemos seu email com mais ninguém.</small>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Senha</label>
+                    <input type="password" class="form-control" id="lblSenha" name="lblSenha" placeholder="Senha" required>
+                  </div>
+                  <div class="form-group">
+                      <label for="exampleInputPassword1">Confirma Senha</label>
+                      <input type="password" class="form-control" id="lblSenha2" name="lblSenha2" placeholder="Senha" required>
+                    </div>
+                  <button type="submit" class="btn btn-primary">Cadastrar</button>
+              </form>
           </div>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email</label>
-            <input type="email" class="form-control" id="lblEmail" name="lblEmail" aria-describedby="emailHelp" placeholder="E-mail" required>
-            <small id="emailHelp" class="form-text text-muted">Nunca compartilhe seu email com mais ninguém.</small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Senha</label>
-            <input type="password" class="form-control" id="lblSenha" name="lblSenha" placeholder="Senha" required>
-          </div>
-          <div class="form-group">
-              <label for="exampleInputPassword1">Confirma Senha</label>
-              <input type="password" class="form-control" id="lblSenha2" name="lblSenha2" placeholder="Senha" required>
-            </div>
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-      </form>
+        </div>
     </div>
-  </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
 Route::get('/', ['as' => 'login', 'uses' => 'LoginController@index']);
-Route::post('/login', ['as' => 'login.post', 'uses' => 'LoginController@login']);
-Route::get('/logout', ['as' => 'logout', 'uses' => 'LoginController@login']);
-
 Route::get('/cadastro', ['as' => 'cadastro', 'uses' => 'LoginController@cadastro']);
-Route::post('/cadastro', ['as' => 'cadastro.post', 'uses' => 'LoginController@cadastrarUser']);
+Route::get('/home', ['as' => 'home','uses' => 'LoginController@home']);
 
-
-Route::group(['middleware' => 'auth'], function(){
-    Route::get('/home', ['as' => 'home','uses' => 'LoginController@home']);
-    Route::get('/noticias', ['as' => 'noticias', 'uses' => 'NoticiasController@index']);
+Route::group(['prefix' => 'noticias'], function(){
+    Route::get('', ['as' => 'noticias', 'uses' => 'NoticiasController@index']);
     Route::get('/nova-noticia', ['as' => 'noticias.nova', 'uses' => 'NoticiasController@novaNoticia']);
     Route::get('/nova/noticia={id}', ['as' => 'pagina.noticia', 'uses' => 'NoticiasController@paginaNoticia']);
 });
